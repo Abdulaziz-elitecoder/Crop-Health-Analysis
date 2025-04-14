7b99c1f116b71beeebe4112d0573366ec77a2dd5
Abdulaziz Elnoti <abdulaziz.muhammed.elnoti@gmail.com>
2025-04-14 02:29:24 +0200

    minor adjustments

--- utils/security.py
+++ utils/security.py
@@ -1,15 +1,14 @@
-# utils.py
 from config import cipher
 from typing import Dict, Any
 import json
 
-# Encrypt metadata before storing
+# Encrypt data before storing
 def encrypt_data(data: Dict[str, Any]) -> str:
     if data is None:
         return None
     return cipher.encrypt(json.dumps(data).encode()).decode()
 
-# Decrypt metadata when retrieving
+# Decrypt data when retrieving
 def decrypt_data(encrypted_data: str) -> Dict[str, Any]:
     if encrypted_data is None:
         return None
